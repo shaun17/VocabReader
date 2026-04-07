5705ac73b05ee2bfe0852d6937721b7da9f02676
wenren <wenren@gmail.com>
2026-04-07 22:14:15 +0800

    chore: initial Xcode project setup via xcodegen

---
+++ VocabReader/VocabReaderApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct VocabReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("VocabReader")
+        }
+    }
+}
---
+++ VocabReaderTests/VocabReaderTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import VocabReader
+
+final class VocabReaderTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
